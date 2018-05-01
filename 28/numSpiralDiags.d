0162b2b1a1900778c8d416f51349960fe3e4eb8f
Rick Kreikebaum <kreikey@users.noreply.github.com>
2018-04-30 17:16:21 -0700

    Added the solution to Problem 28

--- 28/numSpiralDiags.d
+++ 28/numSpiralDiags.d
@@ -1,5 +1,29 @@
 #!/usr/bin/env rdmd -I..
-import std.stdio
+import std.stdio;
+import std.range;
+import std.algorithm;
 
 void main() {
+  auto diags = generate(spiralDiagonalsInit());
+  int n = 1001;
+  diags.take((n - 1) * 2 + 1).sum.writeln();
+}
+
+auto spiralDiagonalsInit() {
+  int i = 0;
+  int n = 1;
+  int stride = 2;
+
+  int spiralDiagonals() {
+    int lastn = n;
+    if (i > 3) {
+      stride += 2;
+      i = 0;
+    }
+    i++;
+    n += stride;
+    return lastn;
+  }
+
+  return &spiralDiagonals;
 }
