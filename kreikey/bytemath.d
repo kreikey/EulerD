337d0ac95bae450ca58be5d6ae371014bd85212b
Rick Kreikebaum <kreikey@users.noreply.github.com>
2019-08-22 17:21:09 -0700

    Finished add, sub, and helper functions for bytemath.d

--- kreikey/bytemath.d
+++ kreikey/bytemath.d
@@ -3,6 +3,15 @@ module kreikey.bytemath;
 import std.range;
 import std.algorithm;
 import std.stdio;
+import std.range;
+import std.array;
+
+alias rstr = (const byte[] value) => value.retro.map!(a => cast(immutable(char))(a + '0')).array();
+alias rbytes = (const char[] value) => value.retro.map!(a => cast(byte)(a - '0')).array();
+alias isNotEqualTo = (const(byte)[] left, const(byte)[] right) => left.compare(right) != 0;
+alias isEqualTo = (const(byte)[] left, const(byte)[] right) => left.compare(right) == 0;
+alias isGreaterThan = (const(byte)[] left, const(byte)[] right) => left.compare(right) > 0;
+alias isLessThan = (const(byte)[] left, const(byte)[] right) => left.compare(right) < 0;
 
 byte[] add(const(byte)[] left, const(byte)[] right) {
   byte[] result;
@@ -36,11 +45,62 @@ byte[] accumulate(ref byte[] left, const(byte)[] right) {
   return left;
 }
 
-byte[] rbytes(const char[] value) {
-  return value.retro.map!(a => cast(byte)(a - '0')).array();
+byte[] sub(const(byte)[] left, const(byte)[] right) {
+  if (left.isLessThan(right))
+    throw new Exception("left is less than right");
+
+  byte[] result = left.dup;
+  result.decumulate(right);
+
+  return result;
+}
+unittest {
+  byte[] left = "456".rbytes();
+  byte[] right = "123".rbytes();
+  string result;
+
+  result = left.sub(right).rstr();
+  assert(result == "333");
+  right = "456".rbytes();
+  result = left.sub(right).rstr();
+  assert(result == "0");
+  right = "457".rbytes();
+  try {
+    left.sub(right);
+  } catch (Exception e) {
+    writeln(e.msg);
+  }
 }
 
-string rstr(const byte[] value) {
-  return value.retro.map!(a => cast(immutable(char))(a + '0')).array();
+byte[] decumulate(ref byte[] left, const(byte)[] right) {
+  if (left.isLessThan(right))
+    throw new Exception("left is less than right");
+
+  byte[] toAdd = new byte[right.length];
+  toAdd.fill(cast(byte)9);
+  toAdd[] -= right[];
+  left.accumulate(toAdd);
+  left.accumulate([1]);
+  left.length--;
+  
+  while(left[$-1] == 0 && left.length > 1)
+    left.length--;
+
+  return left;
+}
+
+int compare(const(byte)[] left, const(byte)[] right) {
+  if (left.length < right.length)
+    return -1;
+  else if (left.length > right.length)
+    return 1;
+
+  for (long i = cast(long)left.length - 1; i >= 0; i--)
+    if (left[i] < right[i])
+      return -1;
+    else if (left[i] > right[i])
+      return 1;
+
+  return 0;
 }
 
