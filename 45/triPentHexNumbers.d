4e3e3aff25d01b5268310352b747bfa8354b114b
Rick Kreikebaum <kreikey@users.noreply.github.com>
2019-11-30 16:59:48 -0800

    Added Problem 45

---
+++ 45/triPentHexNumbers.d
@@ -0,0 +1,34 @@
+#!/usr/bin/env rdmd -i -I..
+
+import std.stdio;
+import std.datetime.stopwatch;
+import std.range;
+import std.algorithm;
+import std.traits;
+import std.parallelism;
+import std.typecons;
+
+void main() {
+  StopWatch timer;
+  timer.start();
+  writeln("Triangular, pentagonal, and hexagonal numbers");
+
+  auto triangulars = recurrence!((a, n) => a[n-1] + n + 1)(1);
+  auto pentagonals = recurrence!((a, n) => a[n-1] + 3*n + 1)(1);
+  auto hexagonals = recurrence!((a, n) => a[n-1] + 4*n + 1)(1);
+
+  auto found = triangulars
+    .merge(pentagonals)
+    .merge(hexagonals)
+    .group
+    //.tee!(a => writefln("%(%s\t%s%)", a))
+    .filter!(a => a[1] == 3)
+    .take(3)
+    .tail(1)
+    .front;
+
+  timer.stop();
+  writefln("The next triangle number after 40755 that is also pentagonal and hexagonal is:\n%s", found[0]);
+  writefln("Finished in %s milliseconds", timer.peek.total!"msecs"());
+}
+
