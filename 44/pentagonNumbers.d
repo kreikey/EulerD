0619b3e961491df871dd0343060081b1c2c7f634
Rick Kreikebaum <kreikey@users.noreply.github.com>
2019-12-01 12:42:58 -0800

    Factored out pentagonals finder into its own function

--- 44/pentagonNumbers.d
+++ 44/pentagonNumbers.d
@@ -6,10 +6,10 @@ import std.range;
 import std.algorithm;
 import std.traits;
 import std.parallelism;
+import std.typecons;
 
-alias PentagonalGenerator = (a, n) => n*(3*n-1)/2;
-alias Pentagonals = typeof(sequence!PentagonalGenerator(0uL));
-//alias Pentagonals = ReturnType!(sequence!(PentagonalGenerator, long));
+alias PentagonalGenerator = (a, n) => (n+1)*(3*(n+1)-1)/2;
+alias Pentagonals = ReturnType!(sequence!(PentagonalGenerator, ulong));
 ReturnType!isPentagonalInit isPentagonal;
 
 static this() {
@@ -20,11 +20,10 @@ void main() {
   StopWatch timer;
   timer.start();
   writeln("Pentagon Numbers");
-  Pentagonals pentagonals = sequence!PentagonalGenerator(0uL).dropOne();
+  Pentagonals pentagonals = sequence!PentagonalGenerator(0uL);
   auto diffDiffGen = recurrence!((a, n) => (n - 2)%3==0 ? 0 : a[n-1]+1)(0, 0, 0, 2, 2);
   auto diffGen = diffDiffGen.cumulativeFold!((a, b) => a + b)(0);
   auto distGen = diffGen.cumulativeFold!((a,b) => a + b)(1);
-  bool done = false;
   long pj = 0;
   long pk = 0;
 
@@ -37,23 +36,11 @@ void main() {
     .joiner
     .find!(a => isPentagonal(a[1] - a[0]) && isPentagonal(a[0] + a[1]))
     .front;
+
+  //auto found = findSpecialPentagonals();
+
   pj = found[0];
   pk = found[1];
-
-  //foreach (i, d; distGen.enumerate()) {
-    //writeln(pentagonals[i]);
-    //foreach (j; iota(i+1, i+d+1)) {
-      //pj = pentagonals[i];
-      //pk = pentagonals[j];
-      //if (isPentagonal(pj + pk) && isPentagonal(pk - pj)) {
-        //writeln("Pentagonal sum and difference found!");
-        //done = true;
-        //break;
-      //}
-    //}
-    //if (done)
-      //break;
-  //}
 
   writefln("The pentagonal numbers found are p: %s and k: %s", pj, pk);
   writefln("The pentagonal sum and difference are sum: %s and difference: %s", pj + pk, pk - pj);
@@ -62,9 +49,7 @@ void main() {
 }
 
 auto isPentagonalInit() {
-  Pentagonals temp = sequence!PentagonalGenerator(0uL);
-  Pentagonals* pentagonals = new Pentagonals();
-  *pentagonals = temp;
+  auto pentagonals = new Pentagonals();
   bool[long] cache = null;
 
   bool isPentagonal(long number) {
@@ -77,4 +62,29 @@ auto isPentagonalInit() {
   }
 
   return &isPentagonal;
+}
+
+Tuple!(long, long) findSpecialPentagonals() {
+  auto pentagonals = Pentagonals();
+  auto diffDiffGen = recurrence!((a, n) => (n - 2)%3==0 ? 0 : a[n-1]+1)(0, 0, 0, 2, 2);
+  auto diffGen = diffDiffGen.cumulativeFold!((a, b) => a + b)(0);
+  auto distGen = diffGen.cumulativeFold!((a,b) => a + b)(1);
+  long pj = 0;
+  long pk = 0;
+
+
+  foreach (i, d; distGen.enumerate()) {
+    //writeln(i, "\t", pentagonals[i]);
+    foreach (j; iota(i+1, i+d+1)) {
+      pj = pentagonals[i];
+      pk = pentagonals[j];
+      if (isPentagonal(pj + pk) && isPentagonal(pk - pj)) {
+        writeln("Pentagonal sum and difference found!");
+        return tuple(pj, pk);
+      }
+    }
+    if (i == short.max)
+      break;
+  }
+  return tuple(-1L, -1L);
 }
