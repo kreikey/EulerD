b6a9308f8e09fc4d39a7132159040b4bb7134d58
Rick Kreikebaum <kreikey@users.noreply.github.com>
2019-12-22 16:07:44 -0800

    Finished Problem 51

--- 51/primeDigitReplacements.d
+++ 51/primeDigitReplacements.d
@@ -4,6 +4,7 @@ import std.stdio;
 import std.datetime.stopwatch;
 import std.range;
 import std.algorithm;
+import std.conv;
 import kreikey.primes;
 import kreikey.intmath;
 import kreikey.combinatorics;
@@ -12,72 +13,104 @@ import std.array;
 alias permutations = kreikey.combinatorics.permutations;
 alias nextPermutation = kreikey.combinatorics.nextPermutation;
 
-void main() {
+typeof(isPrimeInit()) isPrime;
+
+static this() {
+  isPrime = isPrimeInit();
+}
+
+void main(string[] args) {
   StopWatch timer;
   auto primes = new Primes!ulong();
+  int family = 8;
+
+  if (args.length > 1)
+    family = args[1].parse!int();
 
   timer.start();
-  auto numGroups = primes
+
+  auto smallest = primes
     .map!toDigits
     .find!(a => a.length == 2)
-    .until!(a => a.length == 4)
     .chunkBy!((a, b) => a.length == b.length)
-    .map!(a => a.array())
-    .array();
-  //writeln(typeof(numGroups).stringof);
-  //writeln(numGroups);
-  //writeln(numGroups[0]);
-  auto r = replacements(numGroups[$-1]);
-  //writeln(typeof(r).stringof);
-  //r.group
-    //.array
-    //.map!(a => a[1], a => a[0])
-    //.fold!max
-    //.writeln();
-  r.each!()();
+    .map!(a => a.array
+        .maskings
+        .sort
+        .group
+        .array
+        .map!(a => a[1], a => a[0])
+        .array()
+    )
+    .joiner
+    .find!(a => a[0] == family)
+    .tee!writeln()
+    .front[1]
+    .replacements
+    .map!toNumber
+    .find!isPrime
+    .front;
+
+  writefln("The smallest prime that's part of an %s prime family", family);
+  writeln("when part of the number is replaced is:");
+  writeln(smallest);
+
   timer.stop();
   writefln("Finished in %s milliseconds.", timer.peek.total!"msecs"());
 }
 
-uint[][] replacements(uint[][] source) {
+uint[][] maskings(uint[][] source) {
   size_t n = source[0].length;
-  uint[] masks = new uint[n];
-  masks[] = 0;
-  //writeln(masks);
+  uint[] mask = new uint[n];
+  mask[] = 0;
   uint[][] result;
   uint[] row;
   
-  masks[0] = 10;
-  writeln(masks);
-  nextPermutation(masks).writeln();
-  writeln(masks);
-  nextPermutation(masks).writeln();
-  writeln(masks);
-  nextPermutation(masks).writeln();
-  writeln(masks);
-  //foreach (k; iota(1, n)) {
-    ////writeln("k: ", k);
-    //masks[0..k] = 10;
-    ////masks.reverse();
-    //do {
-      //foreach (e; source) {
-        ////writeln(masks);
-        //row = e.dup;
-        //applyMask(row, masks);
-        //result ~= row;
-      //}
-    //} while (masks.nextPermutation());
-    //masks[] = 0;
-  //}
+  foreach (k; iota(1, n)) {
+    mask[$-k..$] = 10;
+    do {
+      foreach (e; source) {
+        row = e.dup;
+        if (applyMask(row, mask))
+          result ~= row;
+      }
+    } while (mask.nextPermutation());
+    mask[] = 0;
+  }
 
-  //writeln(source);
-  //writeln(result);
   return result;
 }
 
-void applyMask(ref uint[] digits, uint[] mask) {
+bool applyMask(ref uint[] digits, uint[] mask) {
+  ulong seedNdx = mask.countUntil(10);
+  uint seed = digits[seedNdx];
+  
+
+  foreach (ref d, m; lockstep(digits, mask)) {
+    if (m == 10 && d != seed)
+      return false;
+  }
+
   foreach (ref d, m; lockstep(digits, mask)) {
     if (m == 10)
-      d = m;
+      d = 10;
   }
+
+  return true;
+}
+
+uint[][] replacements(uint[] digits) {
+  auto ones = iota(0, 10).array();
+  uint[][] result;
+  uint[] row;
+
+  foreach (d; 0..10) {
+    row = digits.dup;
+    if (d == 0 && row[0] == 10)
+      continue;
+    foreach (ref e; row)
+      if (e == 10)
+        e = d;
+    result ~= row;
+  }
+  return result;
 }
