bd1c795b5c662f5b14236ac94f7fda9207c71a5e
Rick Kreikebaum <kreikey@users.noreply.github.com>
2020-02-10 17:13:10 -0800

    Finished Problem 59

--- 59/xorDecryption.d
+++ 59/xorDecryption.d
@@ -11,14 +11,25 @@ import std.range;
 void main() {
   StopWatch timer;
   string plaintext;
-  auto commonWordsArray = ["the", "a", "for", "to", "if", "so", "over", "who", "what", "where", "when", "why", "how"];
-  auto wordsMap = commonWordsArray.zip(repeat(0)).assocArray();
-  writeln(wordsMap);
+  string[] words;
+  int num = 0;
+  int maxcount = 0;
+  string bestkey = "";
+  string bestplaintext = "";
+  auto commonWordsArray = ["the", "then", "than", "a", "an", "and", "or", "not", "for", "to", "if", "else", "so", "over", "who", "what", "where", "when", "why", "how"];
+  ulong sum = 0;
 
+  writeln("Xor decryption");
   timer.start();
 
-  auto codes = readText("p059_cipher.txt").split(',').map!(a => a.to!int()).array();
-  string ciphertext = codes.map!(a => cast(immutable(char))a).array();
+  writeln("working...");
+
+  auto commonWords = commonWordsArray.zip(repeat(0)).assocArray();
+  string ciphertext = readText("p059_cipher.txt")
+    .split(',')
+    .map!(a => a.to!int())
+    .map!(a => cast(immutable(char))a)
+    .array();
 
   auto keys = iota('a', cast(char)('z' + 1))
     .map!(a => iota('a', cast(char)('z' + 1))
@@ -32,24 +43,30 @@ void main() {
     foreach (c, k; lockstep(ciphertext, key.cycle())) {
       plaintext ~= c ^ k;
     }
-    plaintext.split(' ').length.writeln();
+    words = plaintext.split(' ');
+    foreach (word; words) {
+      if (word in commonWords)
+        num++;
+    }
+    if (num > maxcount) {
+      bestkey = key;
+      maxcount = num;
+      bestplaintext = plaintext;
+    }
+    num = 0;
     plaintext = [];
+    words = [];
   }
 
-  //writeln(keys);
-  //writeln(typeof(keys).stringof);
-  //writeln(codes);
-  //writeln(typeof(codes).stringof);
-  //writeln(ciphertext);
+  sum = bestplaintext.sum();
+
+  writefln("best key: %s", bestkey);
+  writefln("words matched: %s", maxcount);
+  writeln("best plaintext:");
+  writeln(bestplaintext);
+  writefln("plaintext ascii code sum: %s", sum);
 
   timer.stop();
 
   writefln("Finished in %s milliseconds.", timer.peek.total!"msecs"());
 }
-
-//auto keyGenInit() {
-  //char[3] key = ['a', 'a', 'a'];
-  //auto keyGen() {
-  //}
-  //return &keyGen;
-//}
