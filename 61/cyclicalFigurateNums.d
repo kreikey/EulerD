f8ee8fd5d4d7be4bf0a8ea84329d4991a9169623
Rick Kreikebaum <kreikey@users.noreply.github.com>
2020-02-27 17:18:03 -0800

    61 WIP: reduced search space to 3.5 million, built up general structure

--- 61/cyclicalFigurateNums.d
+++ 61/cyclicalFigurateNums.d
@@ -12,13 +12,6 @@ void main() {
 
   timer.start();
 
-  //auto triangulars = sequence!((a, n) => n * (n + 1) / 2).dropOne();
-  //auto squares = sequence!((a, n) => n * n).dropOne();
-  //auto pentagonals = sequence!((a, n) => n * (3 * n - 1) / 2).dropOne();
-  //auto hexagonals = sequence!((a, n) => n * (2 * n - 1)).dropOne();
-  //auto heptagonals = sequence!((a, n) => n * (5 * n - 3) / 2).dropOne();
-  //auto octagonals = sequence!((a, n) => n * (3 * n - 2)).dropOne();
-
   auto triangulars = recurrence!((a, n) => a[n-1] + n + 1)(1);
   auto squares = recurrence!((a, n) => a[n-1] + 2*n + 1)(1);
   auto pentagonals = recurrence!((a, n) => a[n-1] + 3*n + 1)(1);
@@ -31,34 +24,76 @@ void main() {
   bool[ulong] isPentagonal = iota(1000uL, 10000).zip(repeat(false)).assocArray();
   bool[ulong] isHexagonal = iota(1000uL, 10000).zip(repeat(false)).assocArray();
   bool[ulong] isHeptagonal = iota(1000uL, 10000).zip(repeat(false)).assocArray();
-  bool[ulong] isOctagonals = iota(1000uL, 10000).zip(repeat(false)).assocArray();
+  bool[ulong] isOctagonal = iota(1000uL, 10000).zip(repeat(false)).assocArray();
 
   auto fourDigitTriangulars = triangulars.tee!(a => isTriangular[a] = true).find!(a => a > 999).until!(a => a >= 10000).array();
   auto fourDigitSquares = squares.tee!(a => isSquare[a] = true).find!(a => a > 999).until!(a => a >= 10000).array();
   auto fourDigitPentagonals = pentagonals.tee!(a => isPentagonal[a] = true).find!(a => a > 999).until!(a => a >= 10000).array();
   auto fourDigitHexagonals = hexagonals.tee!(a => isHexagonal[a] = true).find!(a => a > 999).until!(a => a >= 10000).array();
   auto fourDigitHeptagonals = heptagonals.tee!(a => isHeptagonal[a] = true).find!(a => a > 999).until!(a => a >= 10000).array();
-  auto fourDigitOctagonals = octagonals.tee!(a => isOctagonals[a] = true).find!(a => a > 999).until!(a => a >= 10000).array();
+  auto fourDigitOctagonals = octagonals.tee!(a => isOctagonal[a] = true).find!(a => a > 999).until!(a => a >= 10000).array();
 
-  writeln(fourDigitTriangulars.length);
-  writeln(fourDigitSquares.length);
-  writeln(fourDigitPentagonals.length);
-  writeln(fourDigitHexagonals.length);
-  writeln(fourDigitHeptagonals.length);
-  writeln(fourDigitOctagonals.length);
-  writeln("unmerged length");
-  writeln(fourDigitTriangulars.length + fourDigitSquares.length + fourDigitPentagonals.length + fourDigitHexagonals.length + fourDigitHeptagonals.length + fourDigitOctagonals.length);
+  auto cyclable4DFigurates = merge(fourDigitTriangulars, fourDigitSquares, fourDigitPentagonals, fourDigitHexagonals, fourDigitHeptagonals, fourDigitOctagonals).uniq.filter!mayCycle.array();
 
-  writeln(factorial(6) * fourDigitTriangulars.length * fourDigitSquares.length * fourDigitPentagonals.length * fourDigitHexagonals.length * fourDigitHeptagonals.length * fourDigitOctagonals.length);
+  auto allFiguratesRepresented = allFiguratesRepresentedInit(isTriangular, isSquare, isPentagonal, isHexagonal, isHeptagonal, isOctagonal);
+  writeln(typeof(allFiguratesRepresented).stringof);
 
-  auto fourDigitFigurates = merge(fourDigitTriangulars, fourDigitSquares, fourDigitPentagonals, fourDigitHexagonals, fourDigitHeptagonals, fourDigitOctagonals).uniq.array();
+  auto keys = cyclable4DFigurates.map!(a => a % 100).array.sort.uniq.array();
 
-  writeln(fourDigitFigurates);
-  writeln("merged length");
-  writeln(fourDigitFigurates.length);
+  writeln("chunks");
+  auto cyclables = cyclable4DFigurates
+    .group!((a, b) => a / 100 == b / 100)
+    .map!(a => a[0] / 100)
+    .zip(cyclable4DFigurates
+        .chunkBy!((a, b) => a / 100 == b / 100)
+        .map!array)
+    .assocArray();
+
+  ulong maxLen = keys.map!(a => cyclables[a].length).fold!max();
+  ulong complexity = 1;
+
+  foreach (i, key; keys.enumerate()) {
+    complexity += cyclables[key].length * maxLen ^^ 5;
+    writefln("%s: %s: %s", i, key, cyclables[key]);
+  }
+
+  writeln("complexity: ", complexity);
 
   timer.stop();
 
   writefln("Finished in %s milliseconds.", timer.peek.total!"msecs"());
 }
 
+bool mayCycle(ulong number) {
+  return (number % 100) > 9;
+}
+
+auto allFiguratesRepresentedInit(ref bool[ulong] isTriangular, ref bool[ulong] isSquare, ref bool[ulong] isPentagonal, ref bool[ulong] isHexagonal, ref bool[ulong] isHeptagonal, ref bool[ulong] isOctagonal) {
+  auto allFiguratesRepresented(ulong[] numbers) {
+    if (!numbers.any!(n => isTriangular[n])())
+      return false;
+    if (!numbers.any!(n => isSquare[n])())
+      return false;
+    if (!numbers.any!(n => isPentagonal[n])())
+      return false;
+    if (!numbers.any!(n => isHexagonal[n])())
+      return false;
+    if (!numbers.any!(n => isHeptagonal[n])())
+      return false;
+    return true;
+  }
+  return &allFiguratesRepresented;
+}
+
+ulong[] findSixCyclableFigurates(ulong[] cyclable4DFigurates, ulong[][ulong] cyclables) {
+  ulong[] result; 
+
+  void inner(ulong[] cyclingFigurates, ulong depth) {
+  }
+
+  foreach (figurate; cyclable4DFigurates) {
+    inner([figurate], 1);
+  }
+
+  return result;
+}
