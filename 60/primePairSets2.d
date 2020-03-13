92569a8bf52bdb5b7da7406bee97c926e01460c7
Rick Kreikebaum <kreikey@users.noreply.github.com>
2020-03-12 17:34:35 -0700

    60: Added new solution that finishes in 1.4 minutes with no guessing

---
+++ 60/primePairSets2.d
@@ -0,0 +1,89 @@
+#!/usr/bin/env rdmd -i -I..
+
+import std.stdio;
+import std.datetime.stopwatch;
+import std.range;
+import std.algorithm;
+import std.array;
+import std.conv;
+import std.math;
+import std.file;
+import kreikey.primes;
+import common;
+
+void main() {
+  StopWatch timer;
+  auto p = new Primes!()();
+
+  timer.start();
+
+  writefln("Prime pair sets");
+
+  auto result = smallestPrimePairSet(5);
+
+  timer.stop();
+
+  ulong smallestSum = 0;
+
+  writefln("Finished in %s milliseconds.", timer.peek.total!"msecs"());
+}
+
+auto smallestPrimePairSet(ulong length) {
+  ulong[] result;
+  auto primes = new Primes!()();
+  auto primesList = primes.take(5).array();
+  ulong biggestSum = ulong.max;
+  ulong[][ulong] cattablesTable;
+  ulong topPrime = primesList[$-1];
+  ulong[] cattables;
+
+  ulong[] getCattables(ulong key) {
+    ulong[]* result = key in cattablesTable;
+    if (result != null)
+      return *result;
+    
+    return [];
+  }
+
+  void inner(ulong[] cattablesFound, ulong[] localPrimes, ulong depth) {
+    ulong catsum;
+    if (depth == length) {
+      writeln(cattablesFound);
+      catsum = cattablesFound.sum();
+      if (catsum < biggestSum) {
+        biggestSum = catsum;
+      }
+      result = cattablesFound;
+      return;
+    }
+
+    foreach (p; localPrimes) {
+      if (cattablesFound.map!(a => getCattables(a)).join.count(p) == depth) {
+        inner(cattablesFound ~ p, getCattables(p), depth + 1);
+      }
+      if (catsum + p * (length - depth) > biggestSum)
+        break;
+    }
+  }
+
+  do {
+    foreach (p; primesList) {
+      inner([p], getCattables(p), 1);
+      if (p >= biggestSum / length)
+        break;
+    }
+
+    primesList ~= primes.front;
+    topPrime = primes.front;
+    writeln(topPrime);
+    primes.popFront();
+
+    foreach (p; primesList[0..$-1]) {
+      if (p.catsPrimeWith(topPrime)) {
+        cattablesTable[p] ~= topPrime;
+      }
+    }
+  } while (result.length == 0);
+
+  return result;
+}
