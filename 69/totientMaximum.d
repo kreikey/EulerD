642183c2f87b21f91a4b22506fe2d152756fcbd4
Rick Kreikebaum <kreikey@users.noreply.github.com>
2020-05-22 14:26:09 -0700

    69: Finished

--- 69/totientMaximum.d
+++ 69/totientMaximum.d
@@ -6,40 +6,30 @@ import std.conv;
 import std.range;
 import std.algorithm;
 import std.typecons;
-import std.functional;
 import kreikey.intmath;
 import kreikey.primes;
-import kreikey.util;
-
-//alias distinctPrimeFactors = kreikey.intmath.distinctPrimeFactors1;
 
 void main() {
   StopWatch timer;
   Tuple!(real, uint) maxNOverTotientN = tuple(0, 0);
-  real nOverTotient;
   uint number = 0;
-  uint[][uint] numbersByFactor;
-  uint[] numbersSharingPrimeFactors;
-  uint[] coprimes;
-  uint coprimeCount;
-  uint top = 1_000_001;
+  uint top = 1_000_000;
+  auto primes = new Primes!uint();
 
   timer.start();
 
-  foreach (n; 2..top) {
-    coprimeCount = totient(n);
-    if (n % 10000 == 0)
-      writefln("n: %s, totient: %s", n, coprimeCount);
-    nOverTotient = real(n)/coprimeCount;
-    if (nOverTotient > maxNOverTotientN[0]) {
-      maxNOverTotientN[0] = nOverTotient;
-      maxNOverTotientN[1] = n;
-    }
-  }
+  number = primes
+    .cumulativeFold!((a, b) => a * b)
+    .until!(a => a > top)
+    .tail(1)
+    .front;
+
+  maxNOverTotientN[0] = real(number)/number.totient();
+  maxNOverTotientN[1] = number;
 
   timer.stop();
 
-  writefln("max n/totient(n) from 2 through %s is: %s; n: %s", top - 1, maxNOverTotientN[0], maxNOverTotientN[1]);
+  writefln("max n/totient(n) from 2 through %s is: %s; n: %s", top, maxNOverTotientN.expand);
 
   writefln("Finished in %s milliseconds.", timer.peek.total!"msecs"());
 }
@@ -54,38 +44,16 @@ uint totient(uint number) {
   if (number == 0)
     return 0;
 
-  //writefln("number: %s", number);
   factors = number.distinctPrimeFactors.to!(uint[])();
-  //writeln(factors);
   lowerPtr = factors in maxByDistinctPrimeFactors;
-  //writeln(lowerPtr);
   lower = lowerPtr == null ? tuple(0u, 0u) : *lowerPtr;
-  //writeln(lower);
 
   coprimeCount = lower[1];
 
-  foreach (n; lower[0]+1..number) {
+  foreach (n; lower[0]+1..number)
     coprimeCount += areCoprime(number, n);
-  }
 
   maxByDistinctPrimeFactors[cast(immutable)factors] = tuple(number, coprimeCount);
 
   return coprimeCount;
 }
-
-//bool areCoprime(ulong a, ulong b) {
-  //bool inner(ulong[] a, ulong[] b) {
-    //return setIntersection(a, b).count() == 0;
-  //}
-
-  //auto factorsA = distinctPrimeFactors(a);
-  //auto factorsB = distinctPrimeFactors(b);
-  //if (factorsA < factorsB)
-    //swap(factorsA, factorsB);
-
-  //return memoize!inner(factorsA, factorsB);
-//}
-
-//ulong[] distinctPrimeFactors(ulong source) {
-  //return primeFactors(source).uniq.array();
-//}
