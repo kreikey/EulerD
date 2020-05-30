b2d7b917c7dfe368cce25215e69ba0b569956d4e
Rick Kreikebaum <kreikey@users.noreply.github.com>
2020-05-30 15:43:06 -0700

    69: New totient algorithm works!

--- 69/totientMaximum.d
+++ 69/totientMaximum.d
@@ -15,23 +15,34 @@ void main() {
   uint top = 1_000_000;
   auto primes = new Primes!uint();
 
+  uint tot1;
+  uint tot2;
+
   timer.start();
 
-  nTotientRatio[0] = primes
-    .cumulativeFold!((a, b) => a * b)
-    .until!(a => a > top)
-    .tail(1)
-    .front;
+  //nTotientRatio[0] = primes
+    //.cumulativeFold!((a, b) => a * b)
+    //.until!(a => a > top)
+    //.tail(1)
+    //.front;
 
-  nTotientRatio[1] = nTotientRatio[0].totient();
-  nTotientRatio[2] = real(nTotientRatio[0])/nTotientRatio[1];
+  //nTotientRatio[1] = nTotientRatio[0].totient();
+  //nTotientRatio[2] = real(nTotientRatio[0])/nTotientRatio[1];
 
-  foreach (n; 2..50000) {
-    if (n % 1000 == 0)
-      writeln(n, ": ", totient(n));
-    totient(n);
+  foreach (n; 2..10000) {
+    tot1 = totient2(n);
+    //tot2 = totient2(n);
+    //writefln("totient(%s): %s totient2(%s): %s", n, tot1, n, tot2);
+    //assert(tot1 == tot2);
   }
 
+  foreach (n; iota(2, 10000).retro()) {
+    tot1 = totient2(n);
+    //tot2 = totient2(n);
+    //writefln("totient(%s): %s totient2(%s): %s", n, tot1, n, tot2);
+    //assert(tot1 == tot2);
+  }
+    
   timer.stop();
 
   writefln("n with max totient(n)/n from 2 through %s is:\n%s; totient: %s; ratio: %s", top, nTotientRatio.expand);
@@ -41,26 +52,51 @@ void main() {
 
 uint totient(uint number) {
   uint coprimeCount = 0;
-  //Tuple!(uint, uint)[uint[]] maxByDistinctPrimeFactors;
-  static Tuple!(uint, uint)[uint[]] maxByDistinctPrimeFactors;
-  uint[] factors;
-  Tuple!(uint, uint)* lowerPtr;
-  Tuple!(uint, uint) lower;
 
   if (number == 0)
     return 0;
 
-  factors = number.distinctPrimeFactors.to!(uint[])();
-  lowerPtr = factors in maxByDistinctPrimeFactors;
-  //writefln("\t%s", lowerPtr);
-  lower = lowerPtr == null ? tuple(0u, 0u) : *lowerPtr;
-
-  coprimeCount = lower[1];
-
-  foreach (n; lower[0]+1..number)
+  foreach (n; 1..number)
     coprimeCount += areCoprime(number, n);
 
-  maxByDistinctPrimeFactors[cast(immutable)factors] = tuple(number, coprimeCount);
+  return coprimeCount;
+}
+
+uint totient2(uint number) {
+  uint coprimeCount = number - 1;
+  uint[] factors = distinctPrimeFactors(number).to!(uint[])();
+  uint[] mask = new uint[factors.length];
+  uint mainFactor;
+  uint[] remainingFactors;
+
+  foreach (k; iota(0, mask.length).retro()) {
+    mask[k..$] = 1;
+    do {
+      mainFactor = 1 * zip(factors, mask).filter!(a => a[1]).map!(a => a[0]).fold!((a, b) => a * b)();
+      remainingFactors = zip(factors, mask).filter!(a => a[1] == 0).map!(a => a[0]).array();
+      coprimeCount -= exclusiveMultiples(number, mainFactor, remainingFactors);
+    } while (nextPermutation(mask));
+    mask[] = 0;
+  }
 
   return coprimeCount;
+}
+
+uint exclusiveMultiples(uint number, uint factor, uint[] moreFactors) {
+  uint multiples = (number - 1) / factor;
+  uint[] mask = new uint[moreFactors.length];
+  uint mainFactor;
+  uint[] remainingFactors;
+
+  foreach (k; iota(0, mask.length).retro()) {
+    mask[k..$] = 1;
+    do {
+      mainFactor = factor * zip(moreFactors, mask).filter!(a => a[1]).map!(a => a[0]).fold!((a, b) => a * b)();
+      remainingFactors = zip(moreFactors, mask).filter!(a => a[1] == 0).map!(a => a[0]).array();
+      multiples -= exclusiveMultiples(number, mainFactor, remainingFactors);
+    } while (nextPermutation(mask));
+    mask[] = 0;
+  }
+
+  return multiples;
 }
