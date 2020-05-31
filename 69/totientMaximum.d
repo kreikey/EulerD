5391ec20b9555e0f300d47bd9fe48e479dcdabee
Rick Kreikebaum <kreikey@users.noreply.github.com>
2020-05-31 15:50:59 -0700

    69: totient2 WIP

--- 69/totientMaximum.d
+++ 69/totientMaximum.d
@@ -30,17 +30,17 @@ void main() {
   //nTotientRatio[2] = real(nTotientRatio[0])/nTotientRatio[1];
 
   foreach (n; 2..10000) {
-    tot1 = totient2(n);
-    //tot2 = totient2(n);
-    //writefln("totient(%s): %s totient2(%s): %s", n, tot1, n, tot2);
-    //assert(tot1 == tot2);
+    tot1 = totient(n);
+    tot2 = totient2(n);
+    writefln("totient(%s): %s totient2(%s): %s", n, tot1, n, tot2);
+    assert(tot1 == tot2);
   }
 
   foreach (n; iota(2, 10000).retro()) {
-    tot1 = totient2(n);
-    //tot2 = totient2(n);
-    //writefln("totient(%s): %s totient2(%s): %s", n, tot1, n, tot2);
-    //assert(tot1 == tot2);
+    tot1 = totient(n);
+    tot2 = totient2(n);
+    writefln("totient(%s): %s totient2(%s): %s", n, tot1, n, tot2);
+    assert(tot1 == tot2);
   }
     
   timer.stop();
@@ -63,40 +63,38 @@ uint totient(uint number) {
 }
 
 uint totient2(uint number) {
-  uint coprimeCount = number - 1;
   uint[] factors = distinctPrimeFactors(number).to!(uint[])();
-  uint[] mask = new uint[factors.length];
-  uint mainFactor;
-  uint[] remainingFactors;
 
-  foreach (k; iota(0, mask.length).retro()) {
-    mask[k..$] = 1;
-    do {
-      mainFactor = 1 * zip(factors, mask).filter!(a => a[1]).map!(a => a[0]).fold!((a, b) => a * b)();
-      remainingFactors = zip(factors, mask).filter!(a => a[1] == 0).map!(a => a[0]).array();
-      coprimeCount -= exclusiveMultiples(number, mainFactor, remainingFactors);
-    } while (nextPermutation(mask));
-    mask[] = 0;
-  }
-
-  return coprimeCount;
+  return exclusiveMultiples(number, 1, factors);
 }
 
 uint exclusiveMultiples(uint number, uint factor, uint[] moreFactors) {
   uint multiples = (number - 1) / factor;
   uint[] mask = new uint[moreFactors.length];
   uint mainFactor;
+  uint[] chosenFactors;
   uint[] remainingFactors;
 
   foreach (k; iota(0, mask.length).retro()) {
     mask[k..$] = 1;
     do {
-      mainFactor = factor * zip(moreFactors, mask).filter!(a => a[1]).map!(a => a[0]).fold!((a, b) => a * b)();
-      remainingFactors = zip(moreFactors, mask).filter!(a => a[1] == 0).map!(a => a[0]).array();
+      separate(moreFactors, mask, chosenFactors, remainingFactors);
+      mainFactor = factor * chosenFactors.fold!((a, b) => a * b)();
       multiples -= exclusiveMultiples(number, mainFactor, remainingFactors);
     } while (nextPermutation(mask));
     mask[] = 0;
   }
 
   return multiples;
+}
+
+void separate(uint[] factors, uint[] mask, ref uint[] chosenFactors, ref uint[] remainingFactors) {
+  chosenFactors.length = 0;
+  remainingFactors.length = 0;
+  for (ulong i = 0; i < factors.length; i++) {
+    if (factors[i])
+      chosenFactors ~= factors[i];
+    else
+      remainingFactors ~= factors[i];
+  }
 }
