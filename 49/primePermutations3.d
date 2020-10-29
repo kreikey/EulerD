94c80937119b358d5a641bc4f2831477d8925165
Rick Kreikebaum <kreikey@users.noreply.github.com>
2020-10-29 12:43:44 -0700

    49: Added primePermutations3.d; simpler and more efficient

--- 49/primePermutations3.d
+++ 49/primePermutations3.d
@@ -4,10 +4,14 @@ import std.stdio;
 import std.datetime.stopwatch;
 import std.range;
 import std.algorithm;
+import std.conv;
 import kreikey.primes;
 import kreikey.intmath;
+//import kreikey.bytemath;
 import kreikey.digits;
+import kreikey.util;
 import kreikey.combinatorics;
+import kreikey.linkedlist;
 
 alias permutations = kreikey.combinatorics.permutations;
 typeof(isPrimeInit()) isPrime;
@@ -19,37 +23,41 @@ static this() {
 void main() {
   StopWatch timer;
   auto primes = new Primes!()();
+  ulong[][] primePermutations;
+  ulong cur;
 
   timer.start();
   writeln("Prime Permutations");
   
-  primes
+  auto primesArray = primes
     .find!(a => a > 999)
     .until!(a => a > 9999)
-    .map!(a => a.toDigits
-        .permutations!uint
-        .map!(a => cast(long)toNumber(a))
-        .cache
-        .filter!(a => a.isPrime() && a > 999)
-        .array
-        .sort
-        .uniq
+    .array();
+
+  primePermutations = primesArray
+    .map!(a => a, a => a.toDigits.asort())
+    .array()
+    .sort!((a, b) => a[1] < b[1])
+    .chunkBy!((a, b) => a[1] == b[1])
+    .map!(a => a.map!(b => b[0])
         .array())
-    .cache
-    .filter!(a => a.length > 2)
-    .array
-    .sort
-    .uniq
-    .filterBidirectional!(a => a.any!(b => a.canFind(b + 3330) && a.canFind(b + 3330 * 2))())
+    .array();
+
+  primePermutations = primePermutations
+    .filter!(a => a.any!(b => a.canFind(b + 3330) && a.canFind(b + 3330 * 2))())
     .map!(a => a.find!(b => a.canFind(b + 3330) && a.canFind(b + 3330 * 2))
         .front
         .recurrence!((a, n) => a[n-1] + 3330)
         .take(3)
         .array())
-    .back
-    .map!toDigits
+    .array();
+
+  primePermutations
+    .each!writeln();
+
+  primePermutations[1]
+    .map!(to!string)
     .join
-    .toString
     .writeln();
 
   timer.stop();
