2ce56b0545d3b9e427dda18588f7c5df28f5eb3b
Rick Kreikebaum <kreikey@users.noreply.github.com>
2020-12-20 16:12:52 -0800

    74: Blacklisted cycle members but only sorted members need blacklisting

--- 74/digitFactorialChains4.d
+++ 74/digitFactorialChains4.d
@@ -7,12 +7,14 @@ import std.algorithm;
 import std.range;
 import std.functional;
 import std.concurrency;
+import std.typecons;
 import kreikey.digits;
 import kreikey.intmath;
 import kreikey.util;
 import kreikey.combinatorics;
 
 alias permutations = kreikey.combinatorics.permutations;
+ulong[] specialCycleRoots = [169, 871, 872];
 
 void main() {
   StopWatch timer;
@@ -20,24 +22,33 @@ void main() {
 
   timer.start();
 
+  ulong[][] specialCycles = specialCycleRoots
+    .map!factorialDigitChain
+    .array();
+  bool[uint[]] specialCycleMembers = specialCycles
+    .join
+    .map!(a => a.toDigits.permutations)
+    .join
+    .map!(a => cast(const(uint)[])a)
+    .zip(repeat(true))
+    //.tee!writeln
+    .assocArray();
+
   writeln("Digit factorial chains");
   writefln("The number of digit factorial chains below %s", limit);
   writeln("with 60 non-repeating terms is:");
 
-  auto length = (limit - 1).countDigits();
-
-  auto sortedDigits = new Generator!(uint[])(getSortedDigitsInit(1, length));
+  auto maxDigs = (limit - 1).countDigits();
+  
+  auto sortedDigits = new Generator!(uint[])(getSortedDigitsInit(1, maxDigs));
   sortedDigits
-    //.tee!writeln
-    .map!(a => a.dup)
+    .filter!(a => a !in specialCycleMembers)
     .map!(a => a, factorialDigitChainLength)
-    //.tee!(a => writefln("%(%s %s%)", a))
     .filter!(a => a[1] == 60)
     .tee!(a => writefln("%(%s %s%)", a))
-    .map!(a => a[0])
+    .map!(a => a[0].dup)
     .map!permutations
     .join
-    .array
     .sort
     .uniq
     .filter!(a => a[0] != 0)
@@ -49,10 +60,29 @@ void main() {
   writefln("finished in %s milliseconds", timer.peek.total!"msecs"());
 }
 
+ulong[] factorialDigitChain(ulong source) {
+  ulong[] chain;
+  ulong index = 0;
+  ulong[ulong] factorialSumIndex = [source:index];
+  ulong sum = source.toDigits.map!factorial.sum();
+  index++;
+
+  chain ~= source;
+
+  while (sum !in factorialSumIndex) {
+    chain ~= sum;
+    factorialSumIndex[cast(immutable)sum] = index;
+    sum = sum.toDigits.map!factorial.sum();
+    index++;
+  }
+
+  return chain;
+}
+
 ulong factorialDigitChainLength(uint[] source) {
   ulong index = 0;
-  ulong[uint[]] factorialSumIndex = [source.dup:index];
-  uint[] sumDigs = source.dup.factDigSumDigs();
+  ulong[uint[]] factorialSumIndex = [source:index];
+  uint[] sumDigs = source.factDigSumDigs();
   index++;
 
   while (sumDigs !in factorialSumIndex) {
@@ -65,6 +95,6 @@ ulong factorialDigitChainLength(uint[] source) {
 }
 
 uint[] factDigSumDigs(uint[] source) {
-  return source.dup.map!factorial.sum.toDigits();
+  return source.map!factorial.sum.toDigits();
 }
 
